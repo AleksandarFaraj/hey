ad06bf25af8602a51fc11731c2c6a4250378abf2
Aleksandar Faraj <AleksandarFaraj@users.noreply.github.com>
2019-12-30 11:46:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
+const n = Date();
+app.get('/', (req, res) => res.send('Hello World! '+n));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
